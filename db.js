7657d5436335c38dd38ffd7589935506975cb5d4
BharanitharanK <bharanitharan1117@live.com>
2018-04-04 11:46:19 +0530

    table

--- db.js
+++ db.js
@@ -6,7 +6,6 @@ var options = {
 var pgp = require('pg-promise')(options);
 var connectionString='postgres://ksubinrgfjhojy:0ac6cb36a2d63e697a7d7a12a42bbfa24a4ef561969fbb2ff7c86a71adb14f56@ec2-23-21-166-148.compute-1.amazonaws.com:5432/d8cfi3emqkjkvh';
 var db = pgp(connectionString);
-var data;
 var id,
     name,
     department,
@@ -24,15 +23,16 @@ function getData(callback) {
     console.log('connected');
     var sql = "select * from employee";
     db.any(sql)
-    .then (function (result) {
+    .then (function (data) {
         if (err) throw err;
         console.log('data extracted');
+        result=data;
         callback(null, result);
     })
 }
 function deletebyId(id, callback) {
     var sql = "delete from employee where id=$1"
-    db.none(sql, [id])
+    db.result(sql, [id])
     .then(function (result) {
         if (err) throw err;
         console.log('deleted by id');
@@ -41,7 +41,7 @@ function deletebyId(id, callback) {
 }
 function editbyId(id, name, department, DOB, callback) {
     var sql = "update employee set name=$1,department=$2,DOB=$3 where id=$4";
-    db.any(sql, [name, department, DOB, id])
+    db.none(sql, [name, department, DOB, id])
     .then(function (result) {
         if (err) throw err;
         console.log('updated');
