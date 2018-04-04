6168b1c9579a0c8a3992c8c056e333f6d9fdde53
BharanitharanK <bharanitharan1117@live.com>
2018-04-04 10:19:18 +0530

    postgres

--- db.js
+++ db.js
@@ -1,17 +1,21 @@
-var db = require('pg');
+var promise = require('bluebird');
+var options = {
+  // Initialization Options
+  promiseLib: promise
+};
+var pgp = require('pg-promise')(options);
+var connectionString='postgres://ksubinrgfjhojy:0ac6cb36a2d63e697a7d7a12a42bbfa24a4ef561969fbb2ff7c86a71adb14f56@ec2-23-21-166-148.compute-1.amazonaws.com:5432/d8cfi3emqkjkvh';
+var db = pgp(connectionString);
 var data;
 var id,
     name,
     department,
     DOB;
-var connectionString="postgres://ksubinrgfjhojy:0ac6cb36a2d63e697a7d7a12a42bbfa24a4ef561969fbb2ff7c86a71adb14f56@ec2-23-21-166-148.compute-1.amazonaws.com:5432/d8cfi3emqkjkvh";
-var con = db.createConnection({
- connectionString: connectionString
-});
 function dbInsert(id, name, department, DOB) {
     console.log('connected');
-    var sql = "insert into employee (id,name,department,DOB) values (?,?,?,?)";
-    con.query(sql, [id, name, department, DOB], function (err, result) {
+    var sql = "insert into employee (id,name,department,DOB) values ($1,$2,$3,$4)";
+    db.none(sql, [id, name, department, DOB]).
+    .then(function () {
         if (err) throw err;
         console.log('inserted');
     })
@@ -19,23 +23,26 @@ function dbInsert(id, name, department, DOB) {
 function getData(callback) {
     console.log('connected');
     var sql = "select * from employee";
-    con.query(sql, function (err, result) {
+    db.any(sql)
+    .then (function (result) {
         if (err) throw err;
         console.log('data extracted');
         callback(null, result);
     })
 }
 function deletebyId(id, callback) {
-    var sql = "delete from employee where id=?"
-    con.query(sql, [id], function (err, result) {
+    var sql = "delete from employee where id=$1"
+    db.none(sql, [id])
+    .then(function (result) {
         if (err) throw err;
         console.log('deleted by id');
         callback(null, result);
     })
 }
 function editbyId(id, name, department, DOB, callback) {
-    var sql = "update employee set name=?,department=?,DOB=? where id=?";
-    con.query(sql, [name, department, DOB, id], function (err, result) {
+    var sql = "update employee set name=$1,department=$2,DOB=$3 where id=$4";
+    db.any(sql, [name, department, DOB, id])
+    .then(function (result) {
         if (err) throw err;
         console.log('updated');
         callback(null, result);
