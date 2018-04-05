137a5915e10cf43648e93f02c789a14079c061f8
BharanitharanK <bharanitharan1117@live.com>
2018-04-05 16:00:51 +0530

    socket

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 var promise = require('bluebird');
 var options = {
-  // Initialization Options
-  promiseLib: promise
+    // Initialization Options
+    promiseLib: promise
 };
 var pgp = require('pg-promise')(options);
-var connectionString='postgres://ksubinrgfjhojy:0ac6cb36a2d63e697a7d7a12a42bbfa24a4ef561969fbb2ff7c86a71adb14f56@ec2-23-21-166-148.compute-1.amazonaws.com:5432/d8cfi3emqkjkvh';
+var connectionString = 'postgres://ksubinrgfjhojy:0ac6cb36a2d63e697a7d7a12a42bbfa24a4ef561969fbb2ff7c86a71adb14f56@ec2-23-21-166-148.compute-1.amazonaws.com:5432/d8cfi3emqkjkvh';
 var db = pgp(connectionString);
 var id,
     name,
@@ -14,43 +14,43 @@ function dbInsert(id, name, department, DOB) {
     console.log('connected');
     var sql = "insert into employee (id,name,department,DOB) values ($1,$2,$3,$4)";
     db.none(sql, [id, name, department, DOB])
-    .then(function () {
-        console.log('inserted');
-    })
+        .then(function () {
+            console.log('inserted');
+        })
 }
 function getData(callback) {
     console.log('connected');
     var sql = "select * from employee";
     db.any(sql)
-    .then (function (data) {
-        console.log('data extracted');
-        result=data;
-        callback(null, result);
-    })
+        .then(function (data) {
+            console.log('data extracted');
+            result = data;
+            callback(null, result);
+        })
 }
 function deletebyId(id, callback) {
     var sql = "delete from employee where id=$1"
     db.result(sql, [id])
-    .then(function (result) {
-        console.log('deleted by id');
-        callback(null, result);
-    })
+        .then(function (result) {
+            console.log('deleted by id');
+            callback(null, result);
+        })
 }
 function editbyId(id, name, department, DOB, callback) {
     var sql = "update employee set name=$1,department=$2,DOB=$3 where id=$4";
     db.none(sql, [name, department, DOB, id])
-    .then(function (result) {
-        console.log('updated');
-        callback(null, result);
-    })
+        .then(function (result) {
+            console.log('updated');
+            callback(null, result);
+        })
 }
-function check(id,callback){
+function check(id, callback) {
     var sql = "select * from employee where id = $1";
     db.any(sql, [id])
-    .then(function (data) {
-        console.log('checking id');
-        result=data;
-        callback(null,result);
-    })
+        .then(function (data) {
+            console.log('checking id');
+            result = data;
+            callback(null, result);
+        })
 }
-module.exports = { dbInsert, getData, deletebyId, editbyId,check };
+module.exports = { dbInsert, getData, deletebyId, editbyId, check };
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require("express");
 var bodyParser = require('body-parser');
-var ejs=require('ejs');
-var db=require('./db.js');
-var app=express();
+var ejs = require('ejs');
+var db = require('./db.js');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -10,69 +10,66 @@ app.use(bodyParser.json());
 app.use(express.static('./'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-http.listen(process.env.PORT||8080);
-app.get('/',function(req,res){
-   res.sendFile(__dirname+'./index.html');
+http.listen(process.env.PORT || 8080);
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + './index.html');
 });
-app.post('/data',function(req,res){
-    var id=parseInt(req.body.id),
-        name=req.body.name;
-        department=req.body.department;
-        DOB=req.body.DOB;
-    db.dbInsert(id,name,department,DOB);
+app.post('/data', function (req, res) {
+    var id = parseInt(req.body.id),
+        name = req.body.name;
+    department = req.body.department;
+    DOB = req.body.DOB;
+    db.dbInsert(id, name, department, DOB);
     res.redirect('/view');
 });
-app.get('/view',function(req,res){
-    var data=function(err,result){
-        if(err) throw err;
-        res.render('view',{result:result});
+app.get('/view', function (req, res) {
+    var data = function (err, result) {
+        if (err) throw err;
+        res.render('view', { result: result });
     }
     db.getData(data);
 })
-app.post('/deletebyId',function(req,res){
-    var id=parseInt(req.body.id);
-    var data=function(err,result){
-        if(err) throw err;
+app.post('/deletebyId', function (req, res) {
+    var id = parseInt(req.body.id);
+    var data = function (err, result) {
+        if (err) throw err;
         console.log(result);
         res.redirect('/view');
     }
-    db.deletebyId(id,data);
+    db.deletebyId(id, data);
 })
-app.post('/editbyId',function(req,res){
-    var id=parseInt(req.body.id);
-        res.render('edit',{id:id});
+app.post('/editbyId', function (req, res) {
+    var id = parseInt(req.body.id);
+    res.render('edit', { id: id });
 })
-app.post('/edit',function(req,res){
-    id=req.body.id.replace(/\//g,"");
-    name=req.body.name;
-    department=req.body.department;
-    DOB=req.body.DOB;
-    var data=function(err,result){
-        if(err) throw err;
+app.post('/edit', function (req, res) {
+    id = req.body.id.replace(/\//g, "");
+    name = req.body.name;
+    department = req.body.department;
+    DOB = req.body.DOB;
+    var data = function (err, result) {
+        if (err) throw err;
         console.log(result);
         res.redirect('/view');
     }
-    db.editbyId(id,name,department,DOB,data);
+    db.editbyId(id, name, department, DOB, data);
 })
-io.on('connection',function(socket){
-     var call=function(err,result)
-        {
-            console.log(result.length);
-            if(result.length==0)
-            {
-                socket.emit('success',{});
-            }
-            else
-            {
-                socket.emit('failure',{});
-            }
+io.on('connection', function (socket) {
+    var call = function (err, result) {
+        console.log(result.length);
+        if (result.length == 0) {
+            socket.emit('success', {});
         }
-    socket.on('check',function(id){
+        else {
+            socket.emit('failure', {});
+        }
+    }
+    socket.on('check', function (id) {
         console.log(id);
-        db.check(id,call);
+        db.check(id, call);
     })
-   
-    socket.on('disconnect',function(){
+
+    socket.on('disconnect', function () {
 
     });
 })
